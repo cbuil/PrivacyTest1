5cab1836c613b5a42163ebf2eb3a0482f6fd5a9a
Carlos Buil Aranda <carlos.buil.aranda@gmail.com>
2019-10-14 17:38:06 -0300

    added new queries

---
+++ src/main/java/cl/utfsm/di/RDFDifferentialPrivacySymbolic/Sensitivity.java
@@ -0,0 +1,41 @@
+package cl.utfsm.di.RDFDifferentialPrivacySymbolic;
+
+import symjava.symbolic.Expr;
+
+public class Sensitivity
+{
+    private double sensitivity;
+    private Expr s;
+    private int maxK;
+
+    public Sensitivity(double sensitivity, Expr s)
+    {
+        this.sensitivity = sensitivity;
+        this.s = s;
+    }
+
+    public Expr getS()
+    {
+        return s;
+    }
+
+    public double getSensitivity()
+    {
+        return sensitivity;
+    }
+    
+    public void setSensitivity(double newSensitivity)
+    {
+        this.sensitivity = newSensitivity;
+    }
+    
+    public int getMaxK()
+    {
+        return maxK;
+    }
+    
+    public void setMaxK(int maxK)
+    {
+        this.maxK = maxK;
+    }
+}
