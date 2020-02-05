97529995b317448ea36a7c2614449ede3167af55
Carlos Buil Aranda <carlos.buil.aranda@gmail.com>
2020-02-04 21:46:02 -0300

    modified parallel run

--- src/main/java/cl/utfsm/di/RDFDifferentialPrivacy/Run/RunSparqlQuery.java
+++ src/main/java/cl/utfsm/di/RDFDifferentialPrivacy/Run/RunSparqlQuery.java
@@ -5,8 +5,18 @@
  */
 package cl.utfsm.di.RDFDifferentialPrivacy.Run;
 
-import cl.utfsm.di.RDFDifferentialPrivacy.HdtDataSource;
 import java.util.concurrent.Callable;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFactory;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  *
@@ -15,14 +25,17 @@ import java.util.concurrent.Callable;
 public class RunSparqlQuery implements Callable<String>
 {
 
-   HdtDataSource hdtDataSource;
+   Model triples;
    String queryString;
    String queryFileName;
+   
+   private static Logger logger = LogManager
+            .getLogger(RunSparqlQuery.class.getName());
 
 
-    RunSparqlQuery(HdtDataSource dataSource, String queryString, String queryFileName)
+    RunSparqlQuery(Model dataSource, String queryString, String queryFileName)
     {
-        this.hdtDataSource = dataSource;
+        this.triples = dataSource;
         this.queryString = queryString;
         this.queryFileName = queryFileName;
     }
@@ -30,12 +43,19 @@ public class RunSparqlQuery implements Callable<String>
    @Override
    public String call() 
     { 
-        try
-        { 
-            int result = hdtDataSource.executeCountQuery(queryString);
-            return queryFileName + '\t' + String.valueOf(result);
-  
-        } 
+        Query query = QueryFactory.create(queryString);
+        try (QueryExecution qexec = QueryExecutionFactory.create(query,
+                triples))
+        {
+            ResultSet results = qexec.execSelect();
+            results = ResultSetFactory.copyResults(results);
+            qexec.close();
+             QuerySolution soln = results.nextSolution();
+            RDFNode x = soln.get(soln.varNames().next());
+            int countResult = x.asLiteral().getInt();
+            logger.info("count query result (dataset): " + countResult);
+            return queryFileName + '\t' + String.valueOf(countResult);
+        }
         catch (Exception e) 
         { 
             // Throwing an exception 
